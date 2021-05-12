a9281374029388554e2e1fab008cfa15d3aa115a
GlaucioSales <glauciosales1995@gmail.com>
2021-05-12 15:37:49 -0300

    12.05.21 - ensure EmailErrorStream will no emit duplicated event

--- lib/presentation/presenter/stream_login_presenter.dart
+++ lib/presentation/presenter/stream_login_presenter.dart
@@ -17,7 +17,7 @@ class StreamLoginPresenter {
   var _state = LoginState();
 
   Stream<String> get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError);
+      _controller.stream.map((state) => state.emailError).distinct();
 
   StreamLoginPresenter({@required this.validation});
 
